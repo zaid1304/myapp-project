422fe60e4424f949b6235af70d9173c65e5f61bb
Zaid Khan <zaidkhanraz99@gmail.com>
2021-10-28 21:49:48 +0530

    remived unnasarily report web vitals

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>hello world! mister whats going on
-        hii misterspfogkldfkvmosafijlc,mferifjs.Xfwrifz.dfjerfjsl'dkgj[oeijls'kj[erifj'erkj]]
+      <h1>
+        hello world!
       </h1>
     </div>
   )
