078028c46bbe04036748ff4bd7f5d48e8055c45b
Zaid Khan <zaidkhanraz99@gmail.com>
2021-10-29 10:26:11 +0530

    adding scss files and fonts

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div>
-      <h1>
-        hello world!
-      </h1>
-    </div>
-  )
+ class App extends Component {
+   constructor(){
+   super();
+   console.log('constructor called');
+   }
+   state={
+     Name:"hello zaid how r u",
+     Salutation:"none"
+   }
+   componentDidMount() {
+     console.log('componentdidMount called');
+   }
+   componentDidUpdate(prevprops,prevstate){
+     console.log("componentdidUpdate called");
+   }
+   
+  render() {
+   if(this.state.Salutation!=="Hello")
+      setTimeout(()=>{
+        this.setState({
+          Name:"praveen",
+          Salutation:"yes"
+        });
+      },5000);
+      console.log("render called");
+      return (
+      <div className="hellom">
+        hii {this.state.Name} 
+      </div>
+      )
+  }
 }
-
-export default App
+export default App;
